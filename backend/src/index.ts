6448ddfc5af7ae5dfbf13b0dbdecc9cd7249cedc
dipinrz <dipindas.ah@reizend.ai>
2025-08-12 14:23:43 +0530

    second

---
+++ backend/src/index.ts
@@ -0,0 +1,16 @@
+import express, { type Request, type Response } from "express";
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello from Express + TypeScript!");
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running at http://localhost:${PORT}`);
+});
