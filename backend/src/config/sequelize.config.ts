7900ea1fc4c1d9a34a8288ae9027946aa0bda62d
dipinrz <dipindas.ah@reizend.ai>
2025-08-14 17:35:40 +0530

    all

--- backend/src/config/sequelize.config.ts
+++ backend/src/config/sequelize.config.ts
@@ -1,13 +1,12 @@
-import { Sequelize } from 'sequelize';
-import { jwtDecoder } from '../utils/jst-decode';
+import { Sequelize } from "sequelize";
+import { jwtDecoder } from "../utils/jst-decode";
 
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 const URL = jwtDecoder(process.env.DB_TOKEN as string);
 
 export const sequelizeInstance = new Sequelize(URL as string, {
-  dialect: 'postgres',
+  dialect: "postgres",
   logging: false,
-  sync:{alter: true},
-  
+  sync: { alter: true },
 });
